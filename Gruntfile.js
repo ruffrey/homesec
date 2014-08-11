df1a23b2082a2370d4c6f573e0d4823a3a83a6c4
Jeff Parrish <jeffhparrish@gmail.com>
2014-08-11 09:12:44 -0700

    initial working app commit

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+'use strict';
+exports = module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-node-webkit-builder');
+	grunt.initConfig({
+	  	nodewebkit: {
+	  		options: {
+		      app_name: 'security',
+		      build_dir: './build', // Where the build version of my node-webkit app is saved
+		      mac: true,
+		      win: true,
+		      linux32: true,
+		      linux64: true
+		  },
+		  src: ['./src/**/*'] // Your node-wekit app
+	  	}
+	});
+	grunt.registerTask('default', ['nodewebkit']);
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# use your webcam as a security cam
+
+Requires [grunt](http://gruntjs.com) and [Node](http://nodejs.org).
+
+Building the app:
+
+	git clone https://github.com/ruffrey/homesec
+	cd homesec
+	npm install
+	grunt
+
+Look in the build folder and you'll have apps for mac, windows, and linux.
