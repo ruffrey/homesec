13d57a1f9c8f18391e3c3978c309793057342f6d
Jeff Parrish <jeffhparrish@gmail.com>
2014-08-13 21:13:30 -0700

    png compression is back. automated releases in grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,72 @@
 'use strict';
 exports = module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-node-webkit-builder');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	var pkg = require('./src/package.json');
 	grunt.initConfig({
 	  	nodewebkit: {
-	  		options: {
-		      	platforms: ['win'],
-      		  	macCredits: './src/credits.html',
-		      	buildDir: './build', // Where the build version of my node-webkit app is saved
-		      	version: '0.8.4'
-		  },
-		  src: ['./src/**/*'] // Your node-wekit app
-	  	}
+		  	dev: {
+		  		options: {
+			      	platforms: ['osx'],
+	      		  	macCredits: './src/credits.html',
+			      	buildDir: './build',
+			      	version: '0.8.4'
+			  	},
+		  		src: ['./src/**/*']
+		  	},
+		  	release: {
+		  		options: {
+			      	platforms: ['osx', 'linux32', 'linux64', 'win'],
+	      		  	macCredits: './src/credits.html',
+			      	buildDir: './build',
+			      	version: '0.8.4'
+			  	},
+		  		src: ['./src/**/*']
+		  	}
+	  	},
+	  	compress: {
+		    osx: {
+		        pretty: true,
+		        options: {
+		            archive: './build/Homesec-v' + pkg.version + '-MacOSX.zip',
+		            mode: 'zip'
+		        },
+		        files: [
+		            {expand: true, cwd: './build/Homesec/osx/', src: ['./**/*'] }
+		        ]
+		    },
+		    win: {
+		        pretty: true,
+		        options: {
+		            archive: './build/Homesec-v' + pkg.version + '-Windows.zip',
+		            mode: 'zip'
+		        },
+		        files: [
+		            {expand: true, cwd: './build/Homesec/win/', src: ['./**/*'] }
+		        ]
+		    },
+		    linux32: {
+		        pretty: true,
+		        options: {
+		            archive: './build/Homesec-v' + pkg.version + '-Linux32.zip',
+		            mode: 'zip'
+		        },
+		        files: [
+		            {expand: true, cwd: './build/Homesec/linux32/', src: ['./**/*'] }
+		        ]
+		    },
+		    linux64: {
+		        pretty: true,
+		        options: {
+		            archive: './build/Homesec-v' + pkg.version + '-Linux64.zip',
+		            mode: 'zip'
+		        },
+		        files: [
+		            {expand: true, cwd: './build/Homesec/linux64/', src: ['./**/*'] }
+		        ]
+		    },
+		}
 	});
-	grunt.registerTask('default', ['nodewebkit']);
+	grunt.registerTask('default', ['nodewebkit:dev']);
+	grunt.registerTask('release', ['nodewebkit:release', 'compress']);
 };
