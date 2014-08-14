e76ded5583bf00cdab2aad107b7375d0ab4099d6
ruffrey <jeffhparrish@gmail.com>
2014-08-13 20:03:57 -0700

    fixed and now builds on windows. removed imagemin package and currently there is no image compression.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,10 @@ exports = module.exports = function (grunt) {
 	grunt.initConfig({
 	  	nodewebkit: {
 	  		options: {
-		      	platforms: ['osx', 'win'],
+		      	platforms: ['win'],
       		  	macCredits: './src/credits.html',
 		      	buildDir: './build', // Where the build version of my node-webkit app is saved
-		      	version: '0.10.2'
+		      	version: '0.8.4'
 		  },
 		  src: ['./src/**/*'] // Your node-wekit app
 	  	}
